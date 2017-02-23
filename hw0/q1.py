c077093664927718841ed006e1a43ce0203cb6a4
ctinray <b03902072@ntu.edu.tw>
2017-02-23 23:07:49 +0800

    remove reshape

--- hw0/q1.py
+++ hw0/q1.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument('out', type=str, help='out')
     args = parser.parse_args()
 
-    mA = np.array(list(map(int, open(args.mA).read().strip().split(',')))).reshape(1, -1)
+    mA = np.array([list(map(int, line.strip().split(','))) for line in open(args.mA).read().strip().split('\n')])
     mB = np.array([list(map(int, line.strip().split(','))) for line in open(args.mB).read().strip().split('\n')])
     mC = sorted(list((np.dot(mA, mB)).reshape(1, -1)[0]))
     open(args.out, 'w').write('\n'.join(map(str, mC)))
