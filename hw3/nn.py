da3220aeeb04ff07171b4b919ff844a4743327ff
ctinray <b03902072@ntu.edu.tw>
2017-04-13 14:35:44 +0800

    basically can run

---
+++ hw3/nn.py
@@ -0,0 +1,158 @@
+import tensorflow as tf
+import math
+import numpy as np
+import time
+
+
+class NNModel:
+
+    def _get_var_normal(self, name, shape):
+        intializer = tf.truncated_normal_initializer(stddev=0.2)
+        return tf.get_variable(name, shape,
+                               initializer=intializer)
+
+    def _get_var_const(self, name, shape, val=0.2):
+        value = np.ones(shape) * val
+        intializer = tf.constant_initializer(value)
+        return tf.get_variable(name, shape,
+                               initializer=intializer)
+
+    def _conv2d(self, x, w):
+        return tf.nn.conv2d(x, w, strides=[1, 1, 1, 1], padding='SAME')
+
+    def _max_pool(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME')
+
+    def _avg_pool(self, x):
+        return tf.nn.avg_pool(x, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='VALID')
+
+    def _conv(self, name, x, shape):
+        with tf.variable_scope(name):
+            conv_w = self._get_var_normal(name + '_w', shape)
+            conv_b = self._get_var_const(name + '_b', shape[-1])
+            return tf.nn.relu(self._conv2d(x, conv_w) + conv_b)
+
+    def _fc(self, name, x, shape):
+        with tf.variable_scope(name):
+            fc_w = self._get_var_normal(name + '_w', shape)
+            fc_b = self._get_var_const(name + '_b', shape[-1])
+            return tf.nn.relu(tf.matmul(x, fc_w) + fc_b)
+
+    def _inference(self, X):
+        x_image = tf.reshape(X, [-1, self.img_shape[0], self.img_shape[1], 1])
+        self.conv_shape = [[5, 5, 1, 32], [5, 5, 32, 32]]
+
+        conv0_h = self._conv('conv0', x_image, self.conv_shape[0])
+        pool0_h = self._max_pool(conv0_h)
+        # 24 x 24 x 32
+
+        conv1_h = self._conv('conv1', pool0_h, self.conv_shape[1])
+        pool1_h = self._avg_pool(conv1_h)
+        # 12 x 12 x 32
+
+        conv_last = pool1_h
+
+        self.fc_widths = [1024, 10]
+        last_shape = [-1, 12, 12, 32]  # computed manually...
+        flattern_width = last_shape[1] * last_shape[2] * last_shape[3]
+        flat_h = tf.reshape(conv_last, [-1, flattern_width])
+
+        fc0_h = self._fc('fc0', flat_h, [flattern_width, self.fc_widths[0]])
+        fc1_h = self._fc('fc1', fc0_h, [self.fc_widths[0], self.fc_widths[1]])
+
+        return fc1_h
+
+    def _loss(self, logits, labels):
+        labels = tf.to_int64(labels)
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            labels=labels, logits=logits, name='xentropy')
+        return tf.reduce_mean(cross_entropy, name='xentropy_mean')
+
+    def _train(self, loss, learning_rate):
+        tf.summary.scalar('loss', loss)
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        train_op = optimizer.minimize(loss, global_step=global_step)
+        return train_op
+
+    def _evaluate(self, logits, labels):
+        correct = tf.nn.in_top_k(logits, labels, 1)
+        return tf.reduce_sum(tf.cast(correct, tf.int32))
+
+    def __init__(self, eta=1e-5, nh1=2, batch_size=1,
+                 n_iter=100, img_shape=[48, 48]):
+        self.eta = eta
+        self.nh1 = nh1
+        self.batch_size = batch_size
+        self.n_iter = n_iter
+        self.img_shape = img_shape
+
+    def fit(self, X, y):
+        # calc batch size
+        if self.batch_size > 0:
+            batch_size = self.batch_size
+        else:
+            batch_size = X.shape[0]
+
+        # connect NN
+        X_placeholder = tf.placeholder(tf.float32, shape=(None, X.shape[1]))
+        y_placeholder = tf.placeholder(tf.int32, shape=(None))
+        with tf.variable_scope('nn') as scope:
+            logits = self._inference(X_placeholder)
+            loss = self._loss(logits, y_placeholder)
+            train_op = self._train(loss, self.eta)
+            n_correct = self._evaluate(logits, y_placeholder)
+            accuracy = n_correct / tf.shape(y_placeholder)[0]
+            tf.summary.scalar('accuracy', accuracy)
+
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver()
+        self.sess = tf.Session()
+
+        # log
+        summary = tf.summary.merge_all()
+        summary_writer = tf.summary.FileWriter('./', self.sess.graph)
+
+        self.sess.run(init)
+        # Start the training loop.
+        for i in range(self.n_iter):
+            start_time = time.time()
+
+            # shuffle
+            inds = np.arange(X.shape[0])
+            np.random.shuffle(inds)
+            X = X[inds]
+            y = y[inds]
+
+            for b in range(0, X.shape[0], batch_size):
+                batch_X = X[b: b + batch_size]
+                batch_y = y[b: b + batch_size]
+                feed_dict = {X_placeholder: batch_X, y_placeholder: batch_y}
+                self.sess.run(train_op, feed_dict=feed_dict)
+
+            duration = time.time() - start_time
+            print('iter %d, %d' % (i, duration))
+            self._evaluate(logits, y_placeholder)
+
+            # calc accuracy
+            ain = self.sess.run(accuracy,
+                                feed_dict={X_placeholder: X, y_placeholder: y})
+            print('a in :', ain)
+
+            # tf summary
+            summary_str = self.sess.run(summary,
+                                        feed_dict={X_placeholder: X, y_placeholder: y})
+            summary_writer.add_summary(summary_str, i)
+            summary_writer.flush()
+
+    def predict(self, X):
+        with tf.variable_scope('nn', reuse=True):
+            X_placeholder = tf.placeholder(
+                tf.float32, shape=(None, X.shape[1]))
+            logits = self._inference(X_placeholder)
+            y_ = self.sess.run(tf.argmax(logits, axis=1),
+                               feed_dict={X_placeholder: X})
+
+        return y_
---
+++ hw3/train.py
@@ -0,0 +1,129 @@
+import numpy as np
+import pandas as pd
+import argparse
+import pdb
+import sys
+import traceback
+from nn import NNModel
+
+
+def get_X(csv):
+    data = pd.read_csv(csv)
+    xs = data.as_matrix()
+    return xs
+
+
+def get_Y(y):
+    f = open(y)
+    ys = list(map(int, f.read().split()))
+    ys = np.array(ys).reshape(-1,)
+    return ys
+
+
+def get_XY(csv):
+    xs = []
+    ys = []
+    with open(csv) as f:
+        f.readline()
+        for l in f:
+            cols = l.split(',')
+            ys.append(int(cols[0]))
+            xs.append(list(map(int, cols[1].split())))
+
+    return {'x': np.array(xs), 'y': np.array(ys)}
+
+
+def split_valid(raw, valid_ratio):
+    n_rows = raw['x'].shape[0]
+
+    # shuffle data
+    inds = np.arange(n_rows)
+    np.random.shuffle(inds)
+    raw['x'] = raw['x'][inds]
+    raw['y'] = raw['y'][inds]
+
+    # split data
+    n_valid = int(n_rows * valid_ratio)
+    train_data = {'x': raw['x'][n_valid:], 'y': raw['y'][n_valid:]}
+    valid_data = {'x': raw['x'][:n_valid], 'y': raw['y'][:n_valid]}
+
+    return train_data, valid_data
+
+
+def accuracy(y, y_):
+    return np.sum(y == y_) / y.shape[0]
+
+
+def write_csv(y, filename):
+    f = open(filename, 'w')
+    f.write('id,label\n')
+    for i in range(y.shape[0]):
+        f.write('%d,%d\n' % (i + 1, y[i]))
+
+    f.close()
+
+
+def transform(x):
+    x = x - np.mean(x, axis=1).reshape(-1, 1)
+    x = x / np.linalg.norm(x).reshape(-1, 1) * 100
+    return x
+
+
+def main():
+    parser = argparse.ArgumentParser(description='ML HW2')
+    parser.add_argument('train', type=str, help='train.csv')
+    parser.add_argument('out', type=str, help='outcome')
+    parser.add_argument('--valid_ratio', type=float,
+                        help='ratio of validation data', default=0.2)
+    parser.add_argument('--n_iter', type=int,
+                        help='ratio of validation data', default=100)
+    parser.add_argument('--eta', type=float,
+                        help='learning rate', default=1e-5)
+    parser.add_argument('--alpha', type=float,
+                        help='regularization', default=1e-4)
+    parser.add_argument('--verbose', type=int, help='verbose', default=0)
+    parser.add_argument('--batch_size', type=int,
+                        help='batch size', default=10)
+    parser.add_argument('--model', type=str,
+                        help='model {logistic, pgm}', default='logistic')
+    args = parser.parse_args()
+    raw_train = get_XY(args.train)
+    train, valid = split_valid(raw_train, args.valid_ratio)
+    # test = {'x': get_X(args.x_test)}
+
+    # transform
+    train['x'] = transform(train['x'])
+    valid['x'] = transform(valid['x'])
+    # test['x'] = transform(test['x'])
+
+    # calculate mean, std
+    mean = np.average(train['x'], axis=0)
+    std = np.std(train['x'], axis=0) + 1e-10
+
+    # normalize
+    train['x'] = (train['x'] - mean) / std
+    valid['x'] = (valid['x'] - mean) / std
+    # test['x'] = (test['x'] - mean) / std
+
+    classifier = NNModel(eta=args.eta,
+                         n_iter=args.n_iter, batch_size=args.batch_size)
+
+    classifier.fit(train['x'], train['y'])
+    train['y_'] = classifier.predict(train['x'])
+    print('accuracy train:', accuracy(train['y_'], train['y']))
+
+    valid['y_'] = classifier.predict(valid['x'])
+    print('accuracy valid:', accuracy(valid['y_'], valid['y']))
+
+    # pdb.set_trace()
+    # test['y_'] = classifier.predict(test['x'])
+    # write_csv(test['y_'], args.out)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        type, value, tb = sys.exc_info()
+        traceback.print_exc()
+        pdb.post_mortem(tb)
