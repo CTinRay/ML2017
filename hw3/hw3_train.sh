fdc5fa0024c3a199c70b61e2228313c6a2ba9629
ctinray <b03902072@ntu.edu.tw>
2017-05-01 12:41:18 +0800

    fix severe err in hw3_train.sh

--- hw3/hw3_train.sh
+++ hw3/hw3_train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3.5 train_dnn.py ${1} /dev/null --n_iter 1000 --batch_size 128 --eta 0.001 --valid_ratio 0.1
+python3.5 train.py ${1} /dev/null --n_iter 1000 --batch_size 128 --eta 0.001 --valid_ratio 0.1
