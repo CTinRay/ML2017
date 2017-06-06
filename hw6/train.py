4657531f5e7e5a04c59d70370007ceac41829e9f
ctinray <b03902072@ntu.edu.tw>
2017-06-06 21:31:36 +0800

    add code for bonus

--- hw6/train.py
+++ hw6/train.py
@@ -3,7 +3,7 @@ import sys
 import traceback
 import numpy as np
 import argparse
-from utils import read_train, split_valid
+from utils import read_train, split_valid, get_user_features, get_movie_features
 from mf import MF
 
 
@@ -11,6 +11,8 @@ def main():
     parser = argparse.ArgumentParser(description='ML HW6')
     parser.add_argument('train', type=str, help='train.csv')
     parser.add_argument('model', type=str, help='model to save')
+    parser.add_argument('--user', type=str, help='user.csv')
+    parser.add_argument('--movie', type=str, help='movie.csv')
     parser.add_argument('--valid_ratio', type=float,
                         help='ratio of validation data.', default=0.1)
     parser.add_argument('--n_iters', type=int, default=100,
@@ -31,20 +33,28 @@ def main():
 
     train, valid = split_valid(train_data, args.valid_ratio)
 
-    mean = np.mean(train['y'])
-    std = np.std(train['y'])
+    # mean = np.mean(train['y'])
+    # std = np.std(train['y'])
 
-    train['y'] = (train['y'] - mean) / std
-    valid['y'] = (valid['y'] - mean) / std
+    # train['y'] = (train['y'] - mean) / std
+    # valid['y'] = (valid['y'] - mean) / std
+    user_features = None
+    if args.user:
+        user_features = get_user_features(args.user)
+
+    movie_features = None
+    if args.movie:
+        movie_features = get_movie_features(args.movie)
 
     # start training
     mf = MF(n_iters=args.n_iters,
             lr=args.lr, batch_size=args.batch_size,
-            filename=args.model, d_latent=args.d_latent)
+            filename=args.model, d_latent=args.d_latent,
+            user_features=user_features,
+            movie_features=movie_features)
     mf.fit(train['x'], train['y'], valid)
 
-    print('std = %f' % std)
-    
+
 if __name__ == '__main__':
     try:
         main()
